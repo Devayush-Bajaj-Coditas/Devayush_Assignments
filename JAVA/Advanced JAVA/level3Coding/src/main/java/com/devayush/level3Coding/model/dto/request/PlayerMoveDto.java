47876643314c344d75f25fd5bc76d318e3f6bb63
devayush-bajaj-coditas <devayush.bajaj@coditas.com>
2022-11-14 13:18:09 +0000

    level3Project-

--- JAVA/Advanced JAVA/level3Coding/src/main/java/com/devayush/level3Coding/model/dto/request/PlayerMoveDto.java
+++ JAVA/Advanced JAVA/level3Coding/src/main/java/com/devayush/level3Coding/model/dto/request/PlayerMoveDto.java
@@ -1,8 +1,10 @@
 package com.devayush.level3Coding.model.dto.request;
 
 import lombok.Data;
+import org.springframework.stereotype.Component;
 
 @Data
+@Component
 public class PlayerMoveDto {
      float playerOneMove;
      float playerTwoMove;
--- JAVA/Advanced JAVA/level3Coding/src/main/java/com/devayush/level3Coding/service/MatchServiceImplementation.java
+++ JAVA/Advanced JAVA/level3Coding/src/main/java/com/devayush/level3Coding/service/MatchServiceImplementation.java
@@ -2,6 +2,8 @@ package com.devayush.level3Coding.service;
 
 import com.devayush.level3Coding.entity.MatchEntity;
 import com.devayush.level3Coding.entity.PlayerEntity;
+import com.devayush.level3Coding.exception.PositionOccupiedException;
+import com.devayush.level3Coding.model.dto.request.PlayerMoveDto;
 import com.devayush.level3Coding.model.dto.request.StartMatchDto;
 import com.devayush.level3Coding.repository.MatchRepository;
 import com.devayush.level3Coding.repository.PlayerRepository;
@@ -18,7 +20,7 @@ public class MatchServiceImplementation implements MatchService {
     PlayerRepository playerRepository;
 
     @Autowired
-    PlayerEntity playerEntity;
+    PlayerMoveDto playerMoveDto;
 
     @Autowired
     MatchEntity matchEntity;
@@ -30,6 +32,8 @@ public class MatchServiceImplementation implements MatchService {
     public MatchEntity createNewMatch(StartMatchDto startMatchDto){
         PlayerEntity playerOne = playerRepository.findById(startMatchDto.getPlayerOneId()).orElse(null);
         PlayerEntity playerTwo = playerRepository.findById(startMatchDto.getPlayerTwoId()).orElse(null);
+        //if(playerOne.getPlayerStatus() || playerTwo.getPlayerStatus().equalsIgnoreCase("In a game")){
+
         playerOne.setPlayerStatus("In a game");
         playerTwo.setPlayerStatus("in a game");
         List<PlayerEntity> playersEnrolled = new ArrayList<>();
@@ -40,6 +44,15 @@ public class MatchServiceImplementation implements MatchService {
         playerRepository.save(playerOne);
         playerRepository.save(playerTwo);
         return matchRepository.save(matchEntity);
+    }
+
+
+    public PlayerMoveDto playerMove (int playerOneMove, int playerTwoMove){
+        if (playerOneMove == playerTwoMove){
+            throw new PositionOccupiedException()
+        }
+        playerMoveDto.setPlayerOneMove(playerOneMove);
+        playerMoveDto.setPlayerTwoMove(playerTwoMove);
     }
 
 
